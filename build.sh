ab79b1d19b19878350e07c085034a8439357493d
Mohamed Rilwan <mohamed_rilwan@thbs.com>
2020-02-02 00:49:48 +0530

    updated build script

--- build.sh
+++ build.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
-pytest --html=test_report.html
+if [ -d build ]
+then
+	echo "Deleting build/ directory";
+	rm -rf build
+fi
+if [ -d dist ]
+then
+	echo "Deleting dist directory";
+	rm -rf dist
+fi
+if [ -d flaskr.egg-info ]
+then
+	echo "Deleting flaskr.egg-info directory";
+	rm -rf flaskr.egg-info
+fi
+if [ -f test_report.html ]
+then
+	echo "Removing the file test_report.html";
+	rm test_report.html
+fi
+. venv/bin/activate
+pytest --html=test_report.html --self-contained-html
 if [ $? -eq 0 ]
 then
 	echo "All test cases passed"
--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+. venv/bin/activate
 export FLASK_APP=flaskr
 flask run
