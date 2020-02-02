279fa5349254a20150a9e3e0b7722543287c4411
Mohamed Rilwan <mohamed_rilwan@thbs.com>
2020-02-02 09:57:51 +0530

    Modified build script

--- build.sh
+++ build.sh
@@ -23,8 +23,5 @@ fi
 pytest --html=test_report.html --self-contained-html
 if [ $? -eq 0 ]
 then
-	echo "All test cases passed"
 	 python3 setup.py sdist bdist_wheel
-else
-	echo "some test cases have failed"
 fi
